05ff0707ad1f37e9a9d4d534d1b221f08ebc9df1
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-07-24 19:54:31 -0300

    Initial Commit

---
+++ SwiftMapApp/SwiftMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapAppApp.swift
+//  SwiftMapApp
+//
+//  Created by Boris R on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
